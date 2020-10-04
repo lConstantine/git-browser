b326163e0965803ff006063a6cc14b04d980c981
konstantin <k.lebedev89@gmail.com>
2020-10-04 14:12:07 +0400

    Updates

---
+++ precache-manifest.1914b5f64c7481f5372a6915b44b75da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "620c64c7db1a696072177929d35b5eb3",
+    "url": "/git-browser/index.html"
+  },
+  {
+    "revision": "1ba6022819eebbdc63be",
+    "url": "/git-browser/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "43d65ebdb625797b542a",
+    "url": "/git-browser/static/css/main.264070cf.chunk.css"
+  },
+  {
+    "revision": "1ba6022819eebbdc63be",
+    "url": "/git-browser/static/js/2.9fb4c5af.chunk.js"
+  },
+  {
+    "revision": "e4c8325d6171f826423d5cde7680da9a",
+    "url": "/git-browser/static/js/2.9fb4c5af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43d65ebdb625797b542a",
+    "url": "/git-browser/static/js/main.3d3779d9.chunk.js"
+  },
+  {
+    "revision": "ae08c9f470255166818e",
+    "url": "/git-browser/static/js/runtime-main.5277e2f7.js"
+  }
+]);
