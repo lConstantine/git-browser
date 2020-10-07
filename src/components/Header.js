d52be6f10719dc50a3d0599586d9ca4ad7c7cac0
konstantin <k.lebedev89@gmail.com>
2020-10-08 00:48:18 +0400

    inline style and navbar adjustments

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,12 @@ const Header = () => {
   const { userName, repositoryName } = useParams();
 
   return (
-    <nav className="teal lighten-2">
-      <div id="repository-name" className="left hide-on-med-and-down">{repositoryName || userName || "Welcome"}</div>
-        {userName && (<Link className="right hide-on-med-and-down" to="/" id="go-back">Go Home</Link>)}
-        {repositoryName && (<Link to={`/${userName}`} className="right hide-on-med-and-down" id="go-repository-list">Go Back</Link>)}
+    <nav className="nav-wrapper teal lighten-2">
+      <div className="left" style={{'margin-left': '10px'}}>{repositoryName || userName || "Welcome"}</div>
+        <ul className="right hide-on-med-and-down">
+        <li>{userName && (<Link className="right" to="/" id="go-back">Go Home</Link>)}</li>
+        <li>{repositoryName && (<Link className="center" to={`/${userName}`}>Go Back</Link>)}</li>
+        </ul>
     </nav>
   );
 };
